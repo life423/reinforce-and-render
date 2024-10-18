d37f5a651c834513982f54e750a8f6bf41053e6d
Andrew Clark <aiandrew631@gmail.com>
2024-10-18 03:58:52 -0500

    remove extra dir

--- gameplay/main.py
+++ gameplay/main.py
@@ -11,8 +11,6 @@ from gameplay.game_manager import Game
 
 if __name__ == "__main__":
     # from core.config import Config
-
     mode = ["training", "play"]
-
     game = Game()
     game.run(mode[0])
