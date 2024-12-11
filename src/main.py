75ea5e1f67bde43abf8278c6162c1dbe8c9b2b0d
Andrew Clark <aiandrew631@gmail.com>
2024-12-11 09:07:46 -0600

    Updated the launch.json configuration and training_data.json for gameplay improvements.

--- src/main.py
+++ src/main.py
@@ -2,15 +2,12 @@ from gameplay.game import Game
 import os
 import sys
 
-# Add the project root directory to sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-# Set Pygame to use a dummy video driver if running in an environment without a display
-# os.environ["SDL_VIDEODRIlsER"] = "dummy"
 
 if __name__ == "__main__":
     game = Game()
-    game.run()  # No arguments needed
+    game.run()
 
