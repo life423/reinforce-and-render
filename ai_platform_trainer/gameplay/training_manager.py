f296309bde1198a02662770444a6ac2fc1370668
Andrew Clark <aiandrew631@gmail.com>
2024-12-15 03:03:31 -0600

    Updated TrainingManager class and training_data.json. Commented out all code in TrainingManager for temporary deactivation, and modified player, enemy, and missile coordinates in training data.

--- ai_platform_trainer/gameplay/training_manager.py
+++ ai_platform_trainer/gameplay/training_manager.py
@@ -1,49 +1,49 @@
-import pygame
-from noise import pnoise1
+# import pygame
+# from noise import pnoise1
 
 
-class TrainingManager:
-    def __init__(self, player, enemy, screen, data_logger):
-        self.player = player
-        self.enemy = enemy
-        self.screen = screen
-        self.data_logger = data_logger
+# class TrainingManager:
+#     def __init__(self, player, enemy, screen, data_logger):
+#         self.player = player
+#         self.enemy = enemy
+#         self.screen = screen
+#         self.data_logger = data_logger
 
-    def training_update(self):
-        # Increment time to get new noise values for smooth movement
-        self.player.noise_time += 0.01
+#     def training_update(self):
+#         # Increment time to get new noise values for smooth movement
+#         self.player.noise_time += 0.01
 
-        # Update player position using Perlin noise
-        dx_player = pnoise1(self.player.noise_time +
-                            self.player.noise_offset_x) * self.player.step
-        dy_player = pnoise1(self.player.noise_time +
-                            self.player.noise_offset_y) * self.player.step
-        self.player.position["x"] = max(0, min(self.screen.get_width(
-        ) - self.player.size, self.player.position["x"] + dx_player))
-        self.player.position["y"] = max(0, min(self.screen.get_height(
-        ) - self.player.size, self.player.position["y"] + dy_player))
+#         # Update player position using Perlin noise
+#         dx_player = pnoise1(self.player.noise_time +
+#                             self.player.noise_offset_x) * self.player.step
+#         dy_player = pnoise1(self.player.noise_time +
+#                             self.player.noise_offset_y) * self.player.step
+#         self.player.position["x"] = max(0, min(self.screen.get_width(
+#         ) - self.player.size, self.player.position["x"] + dx_player))
+#         self.player.position["y"] = max(0, min(self.screen.get_height(
+#         ) - self.player.size, self.player.position["y"] + dy_player))
 
-        # Update enemy position
-        self.enemy.update_movement()
+#         # Update enemy position
+#         self.enemy.update_movement()
 
-        # Check for collision between player and enemy
-        collision_occurred = self.check_collision()
+#         # Check for collision between player and enemy
+#         collision_occurred = self.check_collision()
 
-        # Log the data for training purposes
-        self.data_logger.log_data(
-            {
-                "timestamp": pygame.time.get_ticks(),
-                "player_position": self.player.position,
-                "enemy_position": self.enemy.pos,
-                "distance": ((self.player.position["x"] - self.enemy.pos["x"]) ** 2 + (self.player.position["y"] - self.enemy.pos["y"]) ** 2) ** 0.5,
-                "collision": collision_occurred
-            }
-        )
+#         # Log the data for training purposes
+#         self.data_logger.log_data(
+#             {
+#                 "timestamp": pygame.time.get_ticks(),
+#                 "player_position": self.player.position,
+#                 "enemy_position": self.enemy.pos,
+#                 "distance": ((self.player.position["x"] - self.enemy.pos["x"]) ** 2 + (self.player.position["y"] - self.enemy.pos["y"]) ** 2) ** 0.5,
+#                 "collision": collision_occurred
+#             }
+#         )
 
-    def check_collision(self):
-        player_rect = pygame.Rect(
-            self.player.position["x"], self.player.position["y"], self.player.size, self.player.size)
-        enemy_rect = pygame.Rect(
-            self.enemy.pos["x"], self.enemy.pos["y"], self.enemy.size, self.enemy.size)
+#     def check_collision(self):
+#         player_rect = pygame.Rect(
+#             self.player.position["x"], self.player.position["y"], self.player.size, self.player.size)
+#         enemy_rect = pygame.Rect(
+#             self.enemy.pos["x"], self.enemy.pos["y"], self.enemy.size, self.enemy.size)
 
-        return player_rect.colliderect(enemy_rect)
+#         return player_rect.colliderect(enemy_rect)
