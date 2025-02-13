bc627bac120607ab71a5f94776249bcb04e4d664
Andrew Clark <aiandrew631@gmail.com>
2025-02-13 02:18:21 -0600

    Updated GitHub Actions workflow to specifically build a Mac application, including steps for setting up Python, installing dependencies, building the app with PyInstaller, creating a .dmg file and uploading it as an artifact. Also refactored 'training_mode.py' by removing unnecessary comments and simplifying logging section.

--- ai_platform_trainer/gameplay/modes/training_mode.py
+++ ai_platform_trainer/gameplay/modes/training_mode.py
@@ -1,4 +1,3 @@
-# FILE: ai_platform_trainer/gameplay/modes/training_mode.py
 
 import math
 import logging
@@ -6,7 +5,6 @@ import random
 import pygame
 from ai_platform_trainer.entities.missile import Missile
 from ai_platform_trainer.utils.helpers import wrap_position
-# If you have other utilities, import them here as needed
 
 
 class TrainingMode:
@@ -23,7 +21,6 @@ class TrainingMode:
             enemy_x = self.game.enemy.pos["x"]
             enemy_y = self.game.enemy.pos["y"]
 
-            # 1) Player & Enemy updates
             self.game.player.update(enemy_x, enemy_y)
             self.game.enemy.update_movement(
                 self.game.player.position["x"],
@@ -31,7 +28,6 @@ class TrainingMode:
                 self.game.player.step,
             )
 
-            # 2) Missile cooldown & chance to fire
             if self.missile_cooldown > 0:
                 self.missile_cooldown -= 1
 
@@ -48,7 +44,6 @@ class TrainingMode:
                         )
                         self.missile_sequences[missile] = []
 
-        # 3) Check missile collisions & lifespan
         current_missiles = self.game.player.missiles[:]
         for missile in current_missiles:
             if missile in self.missile_lifespan:
@@ -61,9 +56,6 @@ class TrainingMode:
                 else:
                     missile.update()
 
-                    # ----------------------
-                    # ENHANCED LOGGING SECTION
-                    # ----------------------
                     if missile in self.missile_sequences:
                         player_x = self.game.player.position["x"]
                         player_y = self.game.player.position["y"]
@@ -71,19 +63,8 @@ class TrainingMode:
                         enemy_y = self.game.enemy.pos["y"]
 
                         # Example distances
-                        # dist_player_enemy = math.hypot(
-                        #     enemy_x - player_x, enemy_y - player_y
-                        # )
-                        # dist_missile_enemy = math.hypot(
-                        #     enemy_x - missile.pos["x"], enemy_y - missile.pos["y"]
-                        # )
 
-                        # If you have vx, vy in your missile, you can compute an angle:
-                        # angle = math.atan2(missile.vy, missile.vx) if you store those in the missile.
 
-                        # This is an example placeholder for model-based action
-                        # missile_angle = 0.0
-                        # missile_action = 0.0
                         
                         missile_angle = math.atan2(missile.vy, missile.vx)
                         missile_action = getattr(missile, "last_action", 0.0)
@@ -98,15 +79,13 @@ class TrainingMode:
                                 "missile_y": missile.pos["y"],
                                 "missile_angle": missile_angle,
                                 
-                                "missile_collision": False,  # Will be updated at finalize
+                                "missile_collision": False,
                                 "missile_action": missile_action,
                                 "timestamp": current_time,
                                 
                             }
                         )
-                    # ----------------------
 
-                    # Collision with enemy
                     if self.game.enemy:
                         enemy_rect = pygame.Rect(
                             self.game.enemy.pos["x"],
@@ -125,9 +104,8 @@ class TrainingMode:
                             self.game.respawn_timer = (
                                 current_time + self.game.respawn_delay
                             )
-                            break  # Only handle one collision per frame
+                            break
 
-                    # Off-screen check
                     if not (
                         0 <= missile.pos["x"] <= self.game.screen_width
                         and 0 <= missile.pos["y"] <= self.game.screen_height
@@ -137,7 +115,6 @@ class TrainingMode:
                         del self.missile_lifespan[missile]
                         logging.debug("Training Mode: Missile left the screen.")
 
-        # 4) Respawn enemy if needed
         if self.game.is_respawning and current_time >= self.game.respawn_timer:
             if self.game.enemy:
                 self.game.handle_respawn(current_time)
